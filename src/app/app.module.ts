80848822a9b50410dd5eff3acbca873c2f8a2ddf
Salih <salih@Salih-MacBook-Pro.local>
2021-05-07 20:58:45 +0300

    layout ve routing olusturuldu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
